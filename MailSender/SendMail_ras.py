fc7549daba64e57fb2bd398c50adedd3d989ae56
choeminjun <choeminjun@naver.com>
2016-11-20 14:39:31 +0900

    upgrade

--- MailSender/SendMail_ras.py
+++ MailSender/SendMail_ras.py
@@ -1,4 +1,4 @@
-import smtplib
+import smtplib, logging, datetime
 
 class MailSenderAPP():
 
@@ -8,17 +8,55 @@ class MailSenderAPP():
         self.Emailsuq = email
         self.Subject = subject
 
+
+    def __loggerSetup__():
+        logging.basicConfig(filename='SendMail_rasLOG.log', level=logging.DEBUG)
+
+
     def EmailSender(self):
-        mailSever = smtplib.SMTP("smtp.gmail.com", 587)
-        mailSever.ehlo()
-        mailSever.starttls()
-        mailSever.login(self.myEmail, self.myPassword)
-        for email in self.Emailsuq:
-            mailSever.sendmail(self.myEmail, email, self.Subject)
+        logging.info('--------Program Starting at:%s.-------------' % (datetime.datetime.now()))
+        try:
+            logging.info('Trying to connect to gmail sever...')
+            mailSever = smtplib.SMTP("smtp.gmail.com", 587)
+            mailSever.ehlo()
+            mailSever.starttls()
+            logging.info('connect to sever:success')
+        except Exception as Error:
+            logging.error('Cant connect to gmail sever. Error messge:' + str(Error))
+            return 'Sever connect Error:' + str(Error)
+
+        try:
+            logging.info('Trying to login With Email and password...')
+            mailSever.login(self.myEmail, self.myPassword)
+            logging.info('logining to gmail sever:success')
+        except Exception as Error:
+            logging.error('Cant login to gmail sever. Error messge:' + str(Error))
+            return 'Login Error:' + str(Error)
+
+
+        try:
+            logging.info('Sending mail to %s...' % (self.Emailsuq))
+            Email_number = 0
+            for email in self.Emailsuq:
+                mailSever.sendmail(self.myEmail, email, self.Subject)
+                Email_number += 1
+            logging.info('Sending mail to %s:success' % (self.Emailsuq))
+        except Exception as Error:
+            logging.info('Cant Send mail to %s. Error messge:' + str(Error) % (self.Emailsuq[Email_number]))
+            return 'Mail sending Error:' + str(Error)
+
 
         mailSever.close()
+        logging.info('-----------Program Exited-------------')
         return True
 
 
-MailSenderAPP('choeminjun5627@gmail.com', input('password:'), \
-            ['choeminjun@naver.com', 'jckcj@naver.com'], 'Subject:hello\nHello?').EmailSender()
+def main():
+    MailSenderAPP.__loggerSetup__()
+    status = MailSenderAPP('choeminjun5627@gmail.com', input('password:'), \
+                          ['choeminjun@naver.com', 'jckcj@naawefver.com'], 'Subject:hello\nHello?').EmailSender()
+
+    return status
+
+status = main()
+print(status)
