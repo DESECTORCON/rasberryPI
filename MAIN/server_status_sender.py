9ae9a03c9d8c2bfc641060738b28d1da363b80fc
choeminjun <choeminjun@naver.com>
2018-09-08 15:48:33 +0900

    Created function - connect_to_server, end_connection

--- MAIN/server_status_sender.py
+++ MAIN/server_status_sender.py
@@ -1,5 +1,7 @@
 from rasberryPI.MailSender.SendMail_ras import MailSenderAPP
 mailsender = MailSenderAPP(my_email='SE.servicesemail.com@gmail.com', my_password=input('give_value   '), )
 mailsender.__loggerSetup__()
+mailsender.connect_to_server()
 statuc = mailsender.EmailSender(['choeminjun@naver.com'], 'Hey')
+mailsender.end_connection()
 print(statuc)
--- MailSender/SendMail_ras.py
+++ MailSender/SendMail_ras.py
@@ -6,12 +6,24 @@ class MailSenderAPP(object):
     def __init__(self, my_email, my_password):
         self.myEmail = my_email
         self.myPassword = my_password
+        self.mailSever = smtplib.SMTP("smtp.gmail.com", 587)
         # Emailsuq = email
         # self.Subject = subject
 
     @staticmethod
     def __loggerSetup__():
         logging.basicConfig(filename='logging.txt', level=logging.DEBUG)
+
+    def connect_to_server(self):
+        try:
+            logging.info('Trying to connect to gmail sever...')
+
+            self.mailSever.ehlo()
+            self.mailSever.starttls()
+            logging.info('connect to sever:success')
+        except Exception as Error:
+            logging.error('Cant connect to gmail sever. Error messge:' + str(Error))
+            return 'Sever connect Error:' + str(Error)
 
     def EmailSender(self, Emailsuq, subject):
         logging.info('--------Program Starting at:%s.-------------' % (datetime.datetime.now()))
@@ -20,18 +32,8 @@ class MailSenderAPP(object):
             return 'Emailsuq have to be a list Error' + str(type(Emailsuq))
 
         try:
-            logging.info('Trying to connect to gmail sever...')
-            mailSever = smtplib.SMTP("smtp.gmail.com", 587)
-            mailSever.ehlo()
-            mailSever.starttls()
-            logging.info('connect to sever:success')
-        except Exception as Error:
-            logging.error('Cant connect to gmail sever. Error messge:' + str(Error))
-            return 'Sever connect Error:' + str(Error)
-
-        try:
             logging.info('Trying to login With Email and password...')
-            mailSever.login(self.myEmail, self.myPassword)
+            self.mailSever.login(self.myEmail, self.myPassword)
             logging.info('logining to gmail sever:success')
         except Exception as Error:
             logging.error('Cant login to gmail sever. Error messge:' + str(Error))
@@ -42,16 +44,20 @@ class MailSenderAPP(object):
             logging.info('Sending mail to %s...' % (Emailsuq))
             Email_number = 0
             for email in Emailsuq:
-                mailSever.sendmail(self.myEmail, email, subject)
+                self.mailSever.sendmail(self.myEmail, email, subject)
                 Email_number += 1
             logging.info('Sending mail to %s:success' % (Emailsuq))
         except Exception as Error:
             logging.info('Cant Send mail to %s. Error messge:'+str(Error))
             return 'Mail sending Error:' + str(Error)
 
-        mailSever.close()
-        logging.info('-----------Program Exited-------------')
+
         return True
+
+    def end_connection(self):
+        self.mailSever.close()
+        logging.info('-----------Program Exited-------------')
+
 #
 #
 # def main(my_email, my_password, email=[], subject='Subject:SE\nTyto alab'):
